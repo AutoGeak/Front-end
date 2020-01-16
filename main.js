59bb69f36508f6748751c60ac574491bc23bfe7c
AutoGeak <36243402+AutoGeak@users.noreply.github.com>
2020-01-16 21:32:34 +0100

    Herkansing

--- main.js
+++ main.js
@@ -41,21 +41,23 @@ var pakket1Price = 10;
 var pakket2Price = 30;
 var pakket3Price = 60;
 
-var pakket1Specs = ['Extra beveiliging tegen diefstal', 'Bescherming tegen het weer', 'Goedgekeurd door verzekering']
-var pakket2Specs = ['Extra beveiliging tegen diefstal', 'Bescherming tegen het weer', 'Goedgekeurd door verzekering', 'Ankerpunt aan de muur']
-var pakket3Specs = ['Extra beveiliging tegen diefstal', 'Bescherming tegen het weer', 'Goedgekeurd door verzekering', 'Ankerpunt aan de muur', '24/7 Camera toezicht']
+var pakket1Specs = ['Extra beveiliging tegen diefstal', 'Bescherming tegen het weer', 'Goedgekeurd door verzekering'];
+var pakket2Specs = ['Extra beveiliging tegen diefstal', 'Bescherming tegen het weer', 'Goedgekeurd door verzekering', 'Ankerpunt aan de muur'];
+var pakket3Specs = ['Extra beveiliging tegen diefstal', 'Bescherming tegen het weer', 'Goedgekeurd door verzekering', 'Ankerpunt aan de muur', '24/7 Camera toezicht'];
 var params = window.location.search;
 params = params.replace("?", "");
 
 var paragraph = document.getElementById("page-title");
 
-// paragraph.textContent += params;
-
 if (params === 'pakket1') {
     var pakket1Ul = document.getElementById("specs-list");
-    var price_months = document.getElementById('price')
+    var price_months = document.getElementById('price');
     price_months.textContent += pakket1Price;
     console.log(price_months);
+    document.getElementById("label1").classList.add("label-active");
+    document.getElementById("label2").classList.add("label-inactive");
+    document.getElementById("label3").classList.add("label-inactive");
+    
 
     pakket1Specs.forEach(function(p) {
         console.log(p);
@@ -72,8 +74,11 @@ if (params === 'pakket1') {
 
 if (params === 'pakket2') {
     var pakket1Ul = document.getElementById("specs-list");
-    var price_months = document.getElementById('price')
+    var price_months = document.getElementById('price');
     price_months.textContent += pakket2Price;
+    document.getElementById("label1").classList.add("label-inactive");
+    document.getElementById("label2").classList.add("label-active");
+    document.getElementById("label3").classList.add("label-inactive");
     pakket2Specs.forEach(function(p) {
         console.log(p);
         var li = document.createElement('li');
@@ -87,8 +92,11 @@ if (params === 'pakket2') {
 
 if (params === 'pakket3') {
     var pakket1Ul = document.getElementById("specs-list");
-    var price_months = document.getElementById('price')
+    var price_months = document.getElementById('price');
     price_months.textContent += pakket3Price;
+    document.getElementById("label1").classList.add("label-inactive");
+    document.getElementById("label2").classList.add("label-inactive");
+    document.getElementById("label3").classList.add("label-active");
 
     pakket3Specs.forEach(function(p) {
         console.log(p);
@@ -97,39 +105,6 @@ if (params === 'pakket3') {
         pakket1Ul.appendChild(li);
         li.innerHTML = li.innerHTML + p;
     });
-}
-
-
-function priceSelector() {
-    var months = document.getElementById("priceSelector").value
-    var priceValue = '';
-    if (params === 'pakket1') {
-        var oldPrice = pakket1Price;
-        priceValue = pakket1Price * months
-        var newPrice = parseInt(priceValue) + parseInt(oldPrice);
-        console.log(newPrice);
-        price_months.innerHTML = newPrice;
-    }
-
-    if (params === 'pakket2') {
-        var oldPrice = pakket2Price;
-        priceValue = pakket2Price * months
-        var newPrice = parseInt(priceValue) + parseInt(oldPrice);
-        console.log(newPrice);
-        price_months.innerHTML = newPrice;
-    }
-
-    if (params === 'pakket3') {
-        var oldPrice = pakket3Price;
-        priceValue = pakket3Price * months
-        var newPrice = parseInt(priceValue) + parseInt(oldPrice);
-        console.log(newPrice);
-        price_months.innerHTML = newPrice;
-    }
-}
-
-function handleClick(myRadio) {
-    alert('New value: ' + myRadio.value);
 }
 
 $(".benefit--row").click(function() {
